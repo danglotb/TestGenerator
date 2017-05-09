ea4619549cb1af46db48937c0375a993ed9510a4
danglotb <bdanglot@gmail.com>
2017-05-09 13:37:13 +0200

    refactor: single class to the whole instrumentation

--- src/main/java/fr/inria/testgenerator/instrumentor/Instrumentor.java
+++ src/main/java/fr/inria/testgenerator/instrumentor/Instrumentor.java
@@ -1,7 +1,10 @@
-package fr.inria.testgenerator;
+package fr.inria.testgenerator.instrumentor;
 
 import spoon.reflect.code.*;
+import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtField;
 import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.declaration.ModifierKind;
 import spoon.reflect.visitor.filter.TypeFilter;
 
 import java.util.List;
@@ -11,7 +14,7 @@ import java.util.List;
  * benjamin.danglot@inria.fr
  * on 08/05/17
  */
-public class FitnessAssignmentInjector {
+public class Instrumentor {
 
     /*
         The target class should have been processed by FitnessInsertion
@@ -44,4 +47,16 @@ public class FitnessAssignmentInjector {
         );
     }
 
+    public static void insertFitnessField(CtClass<?> clazz) {
+        final CtField<Integer> fitnessField = clazz.getFactory().createField();
+        fitnessField.setSimpleName("fitness");
+        fitnessField.addModifier(ModifierKind.PUBLIC);
+        fitnessField.addModifier(ModifierKind.STATIC);
+        fitnessField.setType(clazz.getFactory().Type().INTEGER_PRIMITIVE);
+
+        final CtFieldRead<Integer> fieldRead = clazz.getFactory().createFieldRead();
+        fieldRead.setType(clazz.getFactory().Type().createReference(Integer.class));
+
+        clazz.addField(fitnessField);
+    }
 }
--- src/test/java/fr/inria/testgenerator/instrumentor/TestInstrumentor.java
+++ src/test/java/fr/inria/testgenerator/instrumentor/TestInstrumentor.java
@@ -1,10 +1,12 @@
-package fr.inria.testgenerator;
+package fr.inria.testgenerator.instrumentor;
 
+import org.junit.Before;
 import org.junit.Test;
 import spoon.Launcher;
 import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtMethod;
 
-import static fr.inria.testgenerator.FitnessInsertion.insertFitnessField;
+import static fr.inria.testgenerator.instrumentor.Instrumentor.insertFitnessField;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -12,21 +14,35 @@ import static org.junit.Assert.assertEquals;
  * benjamin.danglot@inria.fr
  * on 08/05/17
  */
-public class TestFitnessInsertion {
+public class TestInstrumentor {
 
-    @Test
-    public void testOnBinarySearch() throws Exception {
-        Launcher launcher = new Launcher();
+    private Launcher launcher;
+
+    @Before
+    public void setUp() throws Exception {
+        launcher = new Launcher();
         launcher.getEnvironment().setAutoImports(true);
         launcher.getEnvironment().setCommentEnabled(true);
 
-        launcher.addInputResource("src/main/java/eu/fbk/se/tcgen2/BinarySearch.java");
+        launcher.addInputResource("src/main/resources/BinarySearch.java");
         launcher.buildModel();
+    }
 
-        final CtClass<Object> clazz = launcher.getFactory().Class().get("BinarySearch");
+    @Test
+    public void testInsertFitnessField() throws Exception {
+        final CtClass<Object> clazz = launcher.getFactory().Class().get("eu.fbk.se.tcgen2.BinarySearch");
         insertFitnessField(clazz);
-
         assertEquals(expectedClass, clazz.toString());
+    }
+
+    @Test
+    public void testInsertFitnessAssignment() throws Exception {
+        final String fullQualifiedName = "eu.fbk.se.tcgen2.BinarySearch";
+        final CtClass<Object> clazz = launcher.getFactory().Class().get(fullQualifiedName);
+        Instrumentor.insertFitnessField(clazz);
+        final CtMethod<?> method = clazz.getMethodsByName("search").get(0);
+        Instrumentor.insertFitnessAssignement(43, method);
+        assertEquals(expectedBody, method.getBody().toString());
     }
 
     private static final String expectedClass = "public class BinarySearch {\n" +
@@ -90,6 +106,53 @@ public class TestFitnessInsertion {
             "        \n" +
             "    }\n" +
             "\n" +
-            "    public static int fitness = Integer.MAX_VALUE;\n" +
+            "    public static int fitness;\n" +
+            "}";
+
+    private static String expectedBody = "{\n" +
+            "    fitness = Math.min(fitness, 8);\n" +
+            "    int first = 0;\n" +
+            "    int last = (array.length) - 1;\n" +
+            "    int middle = (first + last) / 2;\n" +
+            "    boolean sorted = false;\n" +
+            "    if ((array[0]) <= (array[1])) {\n" +
+            "        fitness = Math.min(fitness, 7);\n" +
+            "        if ((array[1]) <= (array[2])) {\n" +
+            "            fitness = Math.min(fitness, 6);\n" +
+            "            if ((array[2]) <= (array[3])) {\n" +
+            "                fitness = Math.min(fitness, 5);\n" +
+            "                if ((array[3]) <= (array[4])) {\n" +
+            "                    fitness = Math.min(fitness, 4);\n" +
+            "                    sorted = true;\n" +
+            "                    while (first <= last) {\n" +
+            "                        fitness = Math.min(fitness, 3);\n" +
+            "                        // element found at index middle\n" +
+            "                        if ((array[middle]) < search) {\n" +
+            "                            fitness = Math.min(fitness, 2);\n" +
+            "                            first = middle + 1;\n" +
+            "                        }// element found at index middle\n" +
+            "                        else {\n" +
+            "                            fitness = Math.min(fitness, 1);\n" +
+            "                            if ((array[middle]) == search) {\n" +
+            "                                fitness = Math.min(fitness, 0);\n" +
+            "                                return middle;\n" +
+            "                            }else {\n" +
+            "                                fitness = Math.min(fitness, 1);\n" +
+            "                                last = middle - 1;\n" +
+            "                            }\n" +
+            "                        }\n" +
+            "                        middle = (first + last) / 2;\n" +
+            "                    } \n" +
+            "                }\n" +
+            "            }\n" +
+            "        }\n" +
+            "    }\n" +
+            "    if (!sorted) {\n" +
+            "        fitness = Math.min(fitness, 2);\n" +
+            "        return -2;\n" +
+            "    }// array not sorted\n" +
+            "    \n" +
+            "    return -1;// element not found\n" +
+            "    \n" +
             "}";
 }
